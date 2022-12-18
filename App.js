168e29d983964b22f5e7c768f7a9b4d2e41e762a
danielbarthel <barthel-daniel@gmx.net>
2022-12-18 21:53:16 +0100

    navigation works

--- App.js
+++ App.js
@@ -1,35 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import {StyleSheet, Text, View, Image, Button, Pressable, Alert} from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
 const d1image = require('./assets/Heimspiel.png')
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+
+function HomeScreen({navigation}) {
     return (
-        <NavigationContainer>
-            {<View style={styles.container}>
-                <View>
-                    <Text style={styles.header}>DJK Nieder-Olm</Text>
-                </View>
-                <View>
-                    <Image source={d1image} style={styles.image}></Image>
-                </View>
-                <View style={styles.footer}>
-                    <Pressable style={styles.button}>
-                        <Text style={styles.buttonText}>Stats</Text>
-                    </Pressable>
-                    <Pressable style={styles.button}>
-                        <Text style={styles.buttonText}>Home</Text>
-                    </Pressable>
-                    <Pressable style={styles.button}>
-                        <Text style={styles.buttonText}>Settings</Text>
-                    </Pressable>
-                </View>
-                <StatusBar style="auto" />
-            </View>}
-        </NavigationContainer>
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.header}>DJK Nieder-Olm</Text>
+            </View>
+            <View>
+                <Image source={d1image} style={styles.image}></Image>
+            </View>
+            <View style={styles.footer}>
+                <Pressable style={styles.button} onPress={() => navigation.navigate('Stats')}>
+                    <Text style={styles.buttonText}>Stats</Text>
+                </Pressable>
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText}>Home</Text>
+                </Pressable>
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText} onPress={() => navigation.navigate('Settings')}>Settings</Text>
+                </Pressable>
+            </View>
+            <StatusBar style="auto" />
+        </View>
     );
 };
+
+function StatsScreen() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.header}>Stats</Text>
+            </View>
+        </View>
+    )
+}
+
+function SettingsScreen() {
+    return (
+        <View>
+            <Text>Settings</Text>
+        </View>
+    )
+}
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Stats" component={StatsScreen} />
+                <Stack.Screen name="Settings" component={SettingsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
 export default App;
+
 
 const styles = StyleSheet.create({
   container: {
